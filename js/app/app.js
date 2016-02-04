b3dffeed13b87180e01eaa6cd806f91435be2a4c
lmarti17 <lucas.martin.78@free.fr>
2016-02-04 03:07:48 +0100

    Add script

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
-console.log('Let\'s get things done !');
+	var $windowHeight = $(window).height();
+
+	$('#map').css('height', $windowHeight - 80);
+	$('#donation').css('height', $windowHeight - 80);
+
+	var $amountPercentage = $('.amount-percentage h2 span').text();
+	$('.level-real').css('width', $amountPercentage);
+	$('.level-real').attr('data-content', $amountPercentage);
+
+	$('.make-donation input[type=number]').on('change', function() {
+
+		var possiblePercentageProgress = $(this).val() / 10;
+		$('.make-donation h2 span').text(possiblePercentageProgress);
+	});
+
+	$('a.contribute-button').on('click', function() {
+		$('.donation-statistics').fadeOut(300);
+		$('.make-donation').fadeIn(1000);
+	})
+
 
 });
