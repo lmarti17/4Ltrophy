aa5bf0cd2a74262c1d1ce462c516db37887b4b87
lmarti17 <lucas.martin.78@free.fr>
2016-02-06 23:47:56 +0100

    Add global variables php, script php daily donation

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$totalDonor0 = 0;
+$totalDonor1 = 0;
+$totalDonor2 = 0;
+$totalDonor3 = 0;
+$totalDonor4 = 0;
+$totalDonor5 = 0;
+$totalDonor6 = 0;
+$totalDonor7 = 0;
+$totalDonor8 = 0;
+
+$totalAmount0 = 0;
+$totalAmount1 = 0;
+$totalAmount2 = 0;
+$totalAmount3 = 0;
+$totalAmount4 = 0;
+$totalAmount5 = 0;
+$totalAmount6 = 0;
+$totalAmount7 = 0;
+$totalAmount8 = 0;
